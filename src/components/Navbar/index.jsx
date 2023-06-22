1c55f9ae1ad5a4e2f7c8d034d0a3edccbfd6d4c7
Adithyan A <deepaadithyan56@gmail.com>
2023-06-22 23:10:20 +0530

    Navbar font changed to glitch

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <header className="bg-slate-800 bg-opacity-30 backdrop-blur-sm p-4 flex items-center justify-center">
       <NavLink to="/">
-        <h1 className="underline font-semibold text-center text-2xl md:text-3xl duration-200 hover:scale-105" style={{color: getRandomColor()}}>
+        <h1 className="underline font-glitch tracking-wider text-center text-2xl md:text-3xl duration-200 hover:scale-105" style={{color: getRandomColor()}}>
           Adi's Sudoku
         </h1>
       </NavLink>
