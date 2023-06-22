a45cd8be6830360a8fe5808c36df3e5e1e17a35c
Adithyan A <deepaadithyan56@gmail.com>
2023-06-22 21:40:20 +0530

    Bagel Fat One font added to tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import HomePage from "./components/HomePage";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
         </Route>
       </Routes>
     </>
