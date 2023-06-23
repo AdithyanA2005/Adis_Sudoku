3caf0cc789c12e47c63e18840d30aa4642d640e0
Adithyan A <deepaadithyan56@gmail.com>
2023-06-23 10:21:58 +0530

    Change cell focus with arrow keys feature added

--- src/components/GamePage/index.jsx
+++ src/components/GamePage/index.jsx
@@ -1,13 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import BoardCell from "./BoardCell";
 import BoardContext from "../../context/board/BoardContext";
 
 export default function GamePage() {
+  const tableRef = useRef(null);
   const { grid } = useContext(BoardContext);
+  const { focusedCell, setFocusedCell } = useContext(BoardContext);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'ArrowUp' && focusedCell.row > 0) setFocusedCell(prev => ({ row: prev.row - 1, col: prev.col }));
+      else if (e.key === 'ArrowDown' && focusedCell.row < 8) setFocusedCell(prev => ({ row: prev.row + 1, col: prev.col }));
+      else if (e.key === 'ArrowLeft' && focusedCell.col > 0) setFocusedCell(prev => ({ row: prev.row, col: prev.col - 1 }));
+      else if (e.key === 'ArrowRight' && focusedCell.col < 8) setFocusedCell(prev => ({ row: prev.row, col: prev.col + 1 }));
+    };
+
+    tableRef.current?.addEventListener('keydown', handleKeyDown);
+    return () => tableRef.current?.removeEventListener('keydown', handleKeyDown);
+  }, [focusedCell]);
 
   return (
-    <section className="">
-      <div className="mt-[5vw] bg-slate-700 shadow-white shadow p-[1px] grid gap-[1px] grid-cols-9 w-full max-w-2xl mx-auto aspect-square">
+    <section>
+      <div
+        ref={tableRef}
+        className="mt-[5vw] bg-slate-700 shadow-white shadow p-[1px] grid gap-[1px] grid-cols-9 w-full max-w-2xl mx-auto aspect-square"
+      >
         {grid.map((row, rowIndex) => {
           return row.map((cell, colIndex) => (
             <BoardCell
