a8646e3ab7e0dbd598cc40106c5528475faa1daf
Adithyan A <deepaadithyan56@gmail.com>
2023-06-23 08:15:21 +0530

    Border with random color added to Layout container

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import { getRandomColor } from "../utils/colors";
 
 export default function Layout() {
   return (
-    <div className="bg-black h-screen">
+    <div style={{borderColor: getRandomColor()}} className="border-4 bg-black h-screen">
       <Navbar />
       <main className="mx-3">
         <Outlet />
