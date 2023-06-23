7f759e9e4efa172b5ffbef3bc07134195b9ccb07
Adithyan A <deepaadithyan56@gmail.com>
2023-06-23 10:27:06 +0530

    Navbar color changed to primary

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 import ColorContext from "../../context/color/ColorContext";
 
 export default function Navbar() {
-  const { secondaryColor } = useContext(ColorContext);
+  const { primaryColor } = useContext(ColorContext);
 
   return (
     <header className="bg-slate-800 bg-opacity-30 backdrop-blur-sm p-4 flex items-center justify-center">
       <NavLink to="/">
         <h1
-          style={{ color: secondaryColor }}
+          style={{ color: primaryColor }}
           className="underline font-glitch tracking-wider text-center text-2xl md:text-3xl duration-200 hover:scale-105"
         >
           Adi's Sudoku
