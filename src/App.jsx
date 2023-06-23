3d9f1a906a99e8ffab6cd8235ab434a2e65fed26
Adithyan A <deepaadithyan56@gmail.com>
2023-06-23 17:42:48 +0530

    Small Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import GamePage from "./components/GamePage";
 
 export default function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/new-game" element={<GamePage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/new-game" element={<GamePage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   )
 }
