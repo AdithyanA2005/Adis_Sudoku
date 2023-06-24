a2ae48d1e6f98516b570bf0abcf0e897da932b0e
Adithyan A <deepaadithyan56@gmail.com>
2023-06-24 13:47:54 +0530

    Animation added for hearts representing lives

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,19 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        scaleJump: "scaleJump 0.5s ease-in-out",
+      },
       fontFamily: {
         "bagel": ["Bagel Fat One", "cursive"],
         "glitch": ["Rubik Glitch", "cursive"],
+      },
+      keyframes: {
+        scaleJump: {
+          "0%": {transform: "scale(0)"},
+          "50%": {transform: "scale(1.5)"},
+          "100%": {transform: "scale(1)"},
+        }
       }
     },
   },
